0453ac390a2439a665b6db15826df6c3defea482
Tom Mason <wheybags@wheybags.com>
2022-07-24 18:35:14 +0200

    polling for input is working!

--- src/XboxControllerDriver.cpp
+++ src/XboxControllerDriver.cpp
@@ -25,6 +25,18 @@ enum class DescriptorType : uint8_t
   InterfacePower,
 };
 
+static void printHex(const void *ptr, uint32_t len)
+{
+	if (ptr == NULL || len == 0) return;
+	const uint8_t *p = (const uint8_t *)ptr;
+	do {
+		if (*p < 16) Serial.print('0');
+		Serial.print(*p++, HEX);
+		Serial.print(' ');
+	} while (--len);
+	Serial.println();
+}
+
 
 struct DescriptorBase
 {
@@ -38,11 +50,9 @@ struct EndpointDescriptor
   DescriptorType bDescriptorType;
   uint8_t bEndpointAddress;
   uint8_t bmAttributes;
-  uint8_t wMaxPacketSize;
+  uint16_t wMaxPacketSize;
   uint8_t bInterval;
 };
-
-
 
 void XboxControllerDriver::init()
 {
@@ -70,28 +80,94 @@ bool XboxControllerDriver::claim(Device_t* device, int type, const uint8_t* desc
 //  constexpr int32_t endpointsStart = 9;
 //  constexpr int32_t endpointDescriptorSize = 7;
 
-
-  int32_t currentDescriptorStartIndex = 0;
-  while (currentDescriptorStartIndex < descriptorsSize)
+  const EndpointDescriptor* endpointIn = nullptr;
+  const EndpointDescriptor* endpointOut = nullptr;
   {
-    const DescriptorBase* descriptor = (const DescriptorBase*)(descriptors + currentDescriptorStartIndex);
-
-    if (descriptor->bDescriptorType == DescriptorType::Endpoint)
+    int32_t currentDescriptorStartIndex = 0;
+    while (currentDescriptorStartIndex < int32_t(descriptorsSize))
     {
-      const EndpointDescriptor* endpoint = (const EndpointDescriptor*)descriptor;
+      const DescriptorBase* descriptor = (const DescriptorBase*) (descriptors + currentDescriptorStartIndex);
 
-      int32_t endpointNumber = endpoint->bEndpointAddress & 0b111;
-      int32_t endpointDirection = (endpoint->bEndpointAddress & 0b10000000) >> 7;
+      if (descriptor->bDescriptorType == DescriptorType::Endpoint)
+      {
+        const EndpointDescriptor* endpoint = (const EndpointDescriptor*) descriptor;
 
-      Serial.printf("Endpoint: bDescriptorType=%d, endpointNumber=%d, endpointDirection=%d, bmAttributes=%x, wMaxPacketSize=%d, bInterval=%d\n",
-                    int(endpoint->bDescriptorType), endpointNumber, endpointDirection, int(endpoint->bmAttributes), int(endpoint->wMaxPacketSize), int(endpoint->bInterval));
+        int32_t endpointNumber = endpoint->bEndpointAddress & 0b111;
+        int32_t endpointDirection = (endpoint->bEndpointAddress & 0b10000000) >> 7;
+
+        Serial.printf("Endpoint: bDescriptorType=%d, endpointNumber=%d, endpointDirection=%d, bmAttributes=%x, wMaxPacketSize=%d, bInterval=%d\n",
+                      int(endpoint->bDescriptorType), endpointNumber, endpointDirection, int(endpoint->bmAttributes), int(endpoint->wMaxPacketSize),
+                      int(endpoint->bInterval));
+
+        if (endpoint->bmAttributes != 0b11) // not an "Interrupt" endpoint
+          continue;
+
+        if (endpointDirection)
+        {
+          release_assert(!endpointIn);
+          endpointIn = endpoint;
+        }
+        else
+        {
+          release_assert(!endpointOut);
+          endpointOut = endpoint;
+        }
+      }
+
+      currentDescriptorStartIndex += descriptor->bLength;
     }
-
-    currentDescriptorStartIndex += descriptor->bLength;
   }
 
-  return false;
+  release_assert(endpointIn && endpointOut);
+
+  this->pipeIn = new_Pipe(device, 3, endpointIn->bEndpointAddress, 1, endpointIn->wMaxPacketSize, endpointIn->bInterval);
+  this->pipeIn->callback_function = pipeInCallbackStatic;
+  this->pipeOut = new_Pipe(device, 3, endpointIn->bEndpointAddress, 0, endpointIn->wMaxPacketSize, endpointIn->bInterval);
+  this->pipeOut->callback_function = pipeOutCallbackStatic;
+
+  memset(&this->reportBuffer, 0, sizeof(XboxInputReport));
+  queue_Data_Transfer(this->pipeIn, &this->reportBuffer, sizeof(XboxInputReport), this);
+
+  return true;
 }
 
 void XboxControllerDriver::disconnect()
 {}
+
+void XboxControllerDriver::pipeInCallbackStatic(const Transfer_t* transfer)
+{
+  Serial.print("XboxControllerDriver::pipeInCallbackStatic\n");
+  if (transfer->driver)
+    ((XboxControllerDriver*)transfer->driver)->pipeInCallback(transfer);
+}
+
+void XboxControllerDriver::pipeOutCallbackStatic(const Transfer_t* transfer)
+{
+  Serial.print("XboxControllerDriver::pipeOutCallbackStatic\n");
+  if (transfer->driver)
+    ((XboxControllerDriver*)transfer->driver)->pipeOutCallback(transfer);
+}
+
+void XboxControllerDriver::pipeInCallback(const Transfer_t* transfer)
+{
+  Serial.print("XboxControllerDriver::pipeInCallback\n");
+  printHex(transfer->buffer, transfer->length);
+  Serial.print("\n-----\n");
+
+  memset(&this->reportBuffer, 0, sizeof(XboxInputReport));
+  queue_Data_Transfer(this->pipeIn, &this->reportBuffer, sizeof(XboxInputReport), this);
+}
+
+void XboxControllerDriver::pipeOutCallback(const Transfer_t* transfer)
+{
+  Serial.print("XboxControllerDriver::pipeOutCallback\n");
+  printHex(transfer->buffer, transfer->length);
+  Serial.print("\n-----\n");
+}
+
+void XboxControllerDriver::control(const Transfer_t* transfer)
+{
+  Serial.print("XboxControllerDriver::control\n");
+  printHex(transfer->buffer, transfer->length);
+  Serial.print("\n-----\n");
+}
--- src/XboxControllerDriver.hpp
+++ src/XboxControllerDriver.hpp
@@ -2,6 +2,26 @@
 #include <Arduino.h>
 #include "USBHost_t36/USBHost_t36.h"
 
+
+// https://github.com/emoose/Xb2XInput/blob/8f4187a23ecd834961151fb68b7a17334820986b/Xb2XInput/XboxController.hpp#L44
+struct OGXINPUT_GAMEPAD
+{
+  uint16_t    wButtons;
+  uint8_t    bAnalogButtons[8];
+  short   sThumbLX;
+  short   sThumbLY;
+  short   sThumbRX;
+  short   sThumbRY;
+};
+
+struct XboxInputReport {
+  uint8_t bReportId;
+  uint8_t bSize;
+
+  OGXINPUT_GAMEPAD Gamepad;
+};
+
+
 class XboxControllerDriver : public USBDriver {
 public:
 	XboxControllerDriver(USBHost &host) { init(); }
@@ -12,6 +32,21 @@ protected:
 	bool claim(Device_t *device, int type, const uint8_t *descriptors, uint32_t len) override;
 	void disconnect() override;
 	void init();
+
+  void control(const Transfer_t *transfer) override;
+
+  static void pipeInCallbackStatic(const Transfer_t* transfer);
+  static void pipeOutCallbackStatic(const Transfer_t* transfer);
+
+  void pipeInCallback(const Transfer_t *transfer);
+  void pipeOutCallback(const Transfer_t* transfer);
+
+private:
+  Pipe_t* pipeIn = nullptr;
+  Pipe_t* pipeOut = nullptr;
+
+  XboxInputReport reportBuffer;
+
 private:
 	Pipe_t mypipes[3] __attribute__ ((aligned(32)));
 	Transfer_t mytransfers[7] __attribute__ ((aligned(32)));
